70f2c5f09626ed203f67f45f530323626149a37c
John Voyatzis <j.voyatzis@satways.net>
2022-01-19 10:54:05 +0200

    added dependencies

--- app/src/main/java/com/jvoyatz/movierama/MovieRamaApplication.kt
+++ app/src/main/java/com/jvoyatz/movierama/MovieRamaApplication.kt
@@ -1,6 +1,13 @@
 package com.jvoyatz.movierama
 
 import android.app.Application
+import timber.log.Timber
 
 class MovieRamaApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+//        if (BuildConfig.DEBUG) {
+//            Timber.plant(Timber.DebugTree())
+//        }
+    }
 }
