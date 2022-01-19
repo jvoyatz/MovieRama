481170945a386ceece98034adf71e80d08eb03e5
John Voyatzis <j.voyatzis@satways.net>
2022-01-19 10:38:06 +0200

    Initial commit

---
+++ app/src/main/java/com/jvoyatz/movierama/MovieRamaApplication.kt
@@ -0,0 +1,6 @@
+package com.jvoyatz.movierama
+
+import android.app.Application
+
+class MovieRamaApplication:Application() {
+}
---
+++ app/src/main/java/com/jvoyatz/movierama/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jvoyatz.movierama.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jvoyatz.movierama.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
