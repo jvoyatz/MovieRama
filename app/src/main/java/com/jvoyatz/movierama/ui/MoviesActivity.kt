e8d76fa51b8133800c9f970cd8ec80a880a5a1c8
John Voyatzis <j.voyatzis@satways.net>
2022-01-20 03:11:27 +0200

    added ui layouts, updated theme

--- app/src/main/java/com/jvoyatz/movierama/ui/MoviesActivity.kt
+++ app/src/main/java/com/jvoyatz/movierama/ui/MoviesActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jvoyatz.movierama.R
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
 
     }
 }
--- app/src/main/java/com/jvoyatz/movierama/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/jvoyatz/movierama/ui/details/DetailsFragment.kt
@@ -1,10 +1,16 @@
 package com.jvoyatz.movierama.ui.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.jvoyatz.movierama.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -38,23 +44,18 @@ class DetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+
+      //  requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        var toolbar: Toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        NavigationUI.setupWithNavController(toolbar, navController)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+       // requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 }
